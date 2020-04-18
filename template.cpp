3dea166b09934c3a98318bce0eeac482c5533d43
pandabcd <smsubhodeep16@gmail.com>
2020-04-19 00:34:53 +0530

    Added template code

---
+++ template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define MOD 1000000007
+
+typedef pair<int, int> PII;
+typedef vector<int> VI;
+typedef vector<string> VS;
+typedef vector<PII> VII;
+typedef vector<VI> VVI;
+typedef map<int,int> MPII;
+typedef set<int> SETI;
+typedef multiset<int> MSETI;
+
+using namespace std;
+
+
+// Use auto
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	return 0;
+}
